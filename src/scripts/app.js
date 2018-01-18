95f5a3f2a2d02d38fccf19828d9d14ed631207ce
Yoshitaka Terazawa <locol6622@gmail.com>
2018-01-18 20:36:26 +0900

    Update env

---
+++ src/scripts/app.js
@@ -0,0 +1,7 @@
+/* eslint-env browser */
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Index from "./Index.js";
+
+ReactDOM.render(<Index />, document.getElementById("app"));
